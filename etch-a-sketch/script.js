0c1f8b0053f9183da3e3ab031f59e246ce8fae75
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-05-13 04:24:06 -0400

    etch a sketch done

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,23 +1,53 @@
+let gridSize;
+const button = document.querySelector(".button");
 const container = document.querySelector(".container");
 
-for (i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+button.addEventListener("click", onPress);
+
+function onPress() {
+ 
+
+  gridSize = prompt("How many squares per side? (Maximum: 100)");
+  gridSize = parseInt(gridSize);
+
+   if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+     alert("Please enter a valid number between 1 and 100.");
+     return;
+   }
+  
+  createGrid(gridSize);
+
+  // Select squares after grid creation
+  const squares = document.querySelectorAll(".square");
+
+  // Add event listeners to grid squares
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", handleMouseEnter);
+    square.addEventListener("mouseleave", handleMouseLeave);
+  });
 }
 
-const square = document.querySelector(".square");
+// creating grid function
+function createGrid(gridSize) {
 
+  container.innerHTML = "";
+  const squareSize = 960 / gridSize;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = squareSize + "px";
+    square.style.height = squareSize + "px";
+    container.appendChild(square);
+  }
+}
+
+// creating hover effects
 function handleMouseEnter() {
-  square.classList.add("hovered"); // Add a class to change the style on hover
+  this.classList.add("hovered");
   console.log("enter");
 }
 
-// Function to handle mouseleave event
 function handleMouseLeave() {
-  square.classList.remove("hovered"); // Remove the class when the mouse leaves
+  this.classList.remove("hovered");
   console.log("exit");
 }
-
-square.addEventListener("mouseover", handleMouseEnter);
-square.addEventListener("mouseout", handleMouseLeave);
