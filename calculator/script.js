2f42b263b3a2d3a7399b19ef9cfb3fc886674586
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-05-20 05:57:40 -0400

    buttons added

---
+++ calculator/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const numberButtons = document.querySelectorAll(".button.number");
+
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log(button.textContent); // or any other action you want to perform
+    });
+  });
+});
+
+
+// button selections
+const addButton = document.querySelector();
+const subtractButton = document.querySelector();
+const multiplyButton = document.querySelector();
+const divideButton = document.querySelector();
+const changeButton = document.querySelector();
+const clearButton = document.querySelector();
+const parenthesisButton = document.querySelector();
+const remainderButton = document.querySelector();
+const equalsButton = document.querySelector();
+
+
+
+
+// operations functions
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function () {};
+
+const multiply = function () {};
+
+const divide = function () {};
+
+const remainder = function () {};
+
+const clear = function () {};
+
+const changeSign = function () {};
+
+const parenthesis = function () {};
+
+const equals = function ()
