e7c975fa1225cd255f64aaa53029c42a36a0b74c
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-09-30 15:23:31 -0400

    html finished

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const myLibrary = []
+const myLibrary = [];
 
-function Book() {
+function Book() {}
 
-}
+function addBookToLibrary() {}
 
-function addBookToLibrary() {
-    
+let addModal = document.getElementById("addModal");
+let closeButton = document.getElementById("close");
+let addButton = document.getElementById("add");
+
+function addBook = () {
+    onclick
 }
