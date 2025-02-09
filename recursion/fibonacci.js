7456a81d5b0827c267ce261ae64a0a3e4bc7ff80
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-02-08 23:33:45 -0500

    added

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -4,11 +4,26 @@ function fib(n) {
 
   let sequence = [0, 1];
   for (let i = 2; i < n; i++) {
-    sequence.push(sequence[i - 1] + seuqnce[i - 2]);
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
   }
   return sequence;
 }
 
-function fibRecursion() {}
+function fibsRec(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  let sequence = fibsRec(n - 1); // Get the sequence up to (n-1)
+  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]); // Add next Fibonacci number
+
+  console.log(`Returning from fibsRec(${n}):`, sequence); // Log the current sequence
+
+  return sequence;
+}
+
+// Test case
+console.log(fibsRec(5));
+
 
 console.log(fib(6))
