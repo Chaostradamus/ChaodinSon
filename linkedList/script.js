fc403c6957e176dcacebf056ee23afc1ccabcc94
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-02-25 05:00:48 -0500

    linked list started

---
+++ linkedList/script.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = Null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+}
