1dc39a4d02afb63144dc20135ad9b8a643f46aba
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-03-05 20:10:08 -0500

    bst started

--- bst/bst.js
+++ bst/bst.js
@@ -1,8 +1,25 @@
 class Node {
-    constructor(data){
-        this.data = data
-        this.right = null
-        this.left = null
-    }
-    
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree([...new Set(array)].sort((a, b) => a - b));
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    let mid = Math.floor(array.length / 2);
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid + 1));
+
+    return root;
+  }
 }
